e3fc22a200e9e9e18767b9f485a23ddf4730113a
Daniil Home <Home@Daniils-MBP.home>
2022-07-27 22:11:18 +0200

    upd

--- main.97fce524d53e3b6274ba.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{466:function(o,p,n){n(467),o.exports=n(833)},513:function(o,p){}},[[466,3,4]]]);
--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,12 +1,3 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js)'],
-  addons: [
-    {
-      name: "storybook-addon-turbo-build",
-      disableSourceMap: true,
-      options: {
-        optimizationLevel: 1,
-      },
-    },
-  ],
+  stories: ['../src/**/*.stories.@(js)']
 }
